5a7025e168498084ff358c32e6759b527ca4cafe
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2024-02-12 14:53:15 -0600

    Update lambda_setup.py

--- data-pipeline/insfraestructure/lambda_setup.py
+++ data-pipeline/insfraestructure/lambda_setup.py
@@ -61,7 +61,60 @@ def create_lambda_function():
 
     return function_name
 
+def add_s3_trigger_to_lambda(lambda_function_name):
+    """
+    Add a trigger to the specified Lambda function to invoke it on S3 PUT events.
+
+    Args:
+    - lambda_function_name: The name of the Lambda function to add the trigger to.
+    """
+    configurations = load_configurations()
+    
+    # Initialize Boto3 client for Lambda with specified region and credentials
+    lambda_client = boto3.client('lambda', 
+                                region_name=configurations["region"],
+                                aws_access_key_id=configurations.get("aws_access_key_id"),
+                                aws_secret_access_key=configurations.get("aws_secret_access_key"))
+
+    # Specify the S3 bucket and event type for the trigger
+    s3_trigger_configuration = {
+        "EventSourceArn": f"arn:aws:s3:::{configurations['bucket_name']}",
+        "Events": ["s3:ObjectCreated:*"],
+        "LambdaFunctionArn": f"arn:aws:lambda:{configurations['region']}:{os.environ['AWS_ACCOUNT_ID']}:function:{lambda_function_name}",
+        "Filter": {
+            "Key": {
+                "FilterRules": [
+                    {
+                        "Name": "suffix",
+                        "Value": ".jpg"
+                    }
+                ]
+            }
+        }
+    }
+
+    # Add the S3 trigger to the Lambda function
+    lambda_client.create_event_source_mapping(
+        FunctionName=lambda_function_name,
+        EventSourceArn=f"arn:aws:s3:::{configurations['bucket_name']}",
+        StartingPosition="LATEST",
+        EventSourceToken="",
+        BatchSize=100,
+        Enabled=True,
+        DestinationConfig={
+            "OnSuccess": {
+                "Destination": f"arn:aws:lambda:{configurations['region']}:{os.environ['AWS_ACCOUNT_ID']}:function:{lambda_function_name}"
+            },
+            "OnFailure": {
+                "Destination": f"arn:aws:lambda:{configurations['region']}:{os.environ['AWS_ACCOUNT_ID']}:function:{lambda_function_name}"
+            }
+        }
+    )
+    print("S3 trigger added to the Lambda function:", lambda_function_name)
+
 if __name__ == "__main__":
-    create_lambda_function()
+    lambda_function_name = create_lambda_function()
+    add_s3_trigger_to_lambda(lambda_function_name)
+
 
 
