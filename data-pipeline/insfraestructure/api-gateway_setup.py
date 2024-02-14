69392bdc54322b38a3dfeb2738591a5c87d42d9f
Diego VillatoroMx <107954399+diegovillatoromx@users.noreply.github.com>
2024-02-14 14:13:28 -0600

    Update api-gateway_setup.py

--- data-pipeline/insfraestructure/api-gateway_setup.py
+++ data-pipeline/insfraestructure/api-gateway_setup.py
@@ -7,6 +7,7 @@ with open('configurations/configurations.json', 'r') as config_file:
     aws_access_key_id = config_data['aws_access_key_id']
     aws_secret_access_key = config_data['aws_secret_access_key']
     aws_region = config_data['region']
+    lambda_api_arn = config_data['lambda_arn']
 
 # Configure API Gateway client using the loaded credentials
 apigateway_client = boto3.client('apigatewayv2',
@@ -37,11 +38,35 @@ config_data['invoke_URL'] = invoke_url
 with open('configurations/configurations.json', 'w') as config_file:
     json.dump(config_data, config_file)
 
-# Create Route for the API
+# Create Integration for the API
+integration_response = apigateway_client.create_integration(
+    ApiId=api_id,
+    IntegrationType='AWS_PROXY',
+    IntegrationUri=lambda_api_arn,
+    IntegrationMethod='POST',
+    PayloadFormatVersion='2.0'
+)
+
+# Create Route for the API with the Integration
 route_response = apigateway_client.create_route(
     ApiId=api_id,
     RouteKey='GET /images',
-    Target='integrations/not_defined_yet'
+    Target=integration_response['IntegrationId']
+)
+
+# Attach permissions to Lambda function for API Gateway to invoke it
+lambda_client = boto3.client('lambda',
+                             aws_access_key_id=aws_access_key_id,
+                             aws_secret_access_key=aws_secret_access_key,
+                             region_name=aws_region)
+
+lambda_client.add_permission(
+    FunctionName='ImageRequestHandler',  # Change this to your Lambda function name
+    StatementId='apigateway-invoke',
+    Action='lambda:InvokeFunction',
+    Principal='apigateway.amazonaws.com',
+    SourceArn=f"arn:aws:execute-api:{aws_region}:{config_data['aws_account_id']}:{api_id}/*/*/*"
 )
 
 print("Invoke URL:", invoke_url)
+
